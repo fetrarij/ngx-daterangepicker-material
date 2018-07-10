d671e8dd99d798894152e29d0dfbb30886756abc
Praveen Puglia <praveenpuglia@gmail.com>
2018-07-10 11:40:35 +0530

    disabling a range if it's beyond minDate or maxDate

--- demo/src/app/custom-ranges/custom-ranges.component.ts
+++ demo/src/app/custom-ranges/custom-ranges.component.ts
@@ -10,20 +10,27 @@ export class CustomRangesComponent implements OnInit {
   selected: any;
   alwaysShowCalendars: boolean;
   maxDate: moment.Moment;
+  minDate: moment.Moment;
   ranges: any = {
-    'Today': [moment(), moment()],
-    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    Today: [moment(), moment()],
+    Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
     'This Month': [moment().startOf('month'), moment().endOf('month')],
-    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-  }
+    'Last Month': [
+      moment()
+        .subtract(1, 'month')
+        .startOf('month'),
+      moment()
+        .subtract(1, 'month')
+        .endOf('month')
+    ]
+  };
   constructor() {
     this.maxDate = moment().add('1 weeks');
+    this.minDate = moment().subtract(3, 'days');
     this.alwaysShowCalendars = true;
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
