6b9f8d3c8f59c1105135a7df77a8a7ac6ef52243
Fetrarijaona R <fetrarij@gmail.com>
2018-11-23 18:02:17 +0300

    feat(clear): when setting value to falsy, datepicker will clear, and added demo
    #11

--- src/daterangepicker/daterangepicker.directive.ts
+++ src/daterangepicker/daterangepicker.directive.ts
@@ -240,7 +240,7 @@ export class DaterangepickerDirective implements OnInit, OnChanges, DoCheck {
         this._el.nativeElement.value = this.picker.chosenLabel;
       }
     } else {
-      //
+      this.picker.clear();
     }
   }
     /**
