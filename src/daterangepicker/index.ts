436680f162b0622a29dcb3530c67014c92dd4e04
veerritu <32728268+veerritu@users.noreply.github.com>
2019-01-29 17:06:47 +0530

    exported localeConfig
    exported localeConfig  so that it can be used outside

--- src/daterangepicker/index.ts
+++ src/daterangepicker/index.ts
@@ -1,3 +1,4 @@
 export { NgxDaterangepickerMd } from './daterangepicker.module';
 export { DaterangepickerComponent } from './daterangepicker.component';
 export { DaterangepickerDirective } from './daterangepicker.directive';
+export { LocaleConfig } from './daterangepicker.config';
