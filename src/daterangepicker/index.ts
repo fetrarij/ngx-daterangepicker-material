2a937b079e383720550b383e811a600611348f93
Fetrarijaona R <fetrarij@gmail.com>
2019-01-30 14:52:29 +0300

    Import localeService

--- src/daterangepicker/index.ts
+++ src/daterangepicker/index.ts
@@ -2,3 +2,4 @@ export { NgxDaterangepickerMd } from './daterangepicker.module';
 export { DaterangepickerComponent } from './daterangepicker.component';
 export { DaterangepickerDirective } from './daterangepicker.directive';
 export { LocaleConfig , LOCALE_CONFIG, DefaultLocaleConfig } from './daterangepicker.config';
+export { LocaleService } from './locale.service';
