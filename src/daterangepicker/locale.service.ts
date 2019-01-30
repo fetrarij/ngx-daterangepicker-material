3c586db1b1dde1670109a1911b3f2ba3840d1416
Fetrarijaona R <fetrarij@gmail.com>
2019-01-30 14:32:37 +0300

    chore(export): add export for LOCALE_CONFIG and DefaultLocaleConfig

--- src/daterangepicker/locale.service.ts
+++ src/daterangepicker/locale.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, Inject } from '@angular/core';
-import { LOCALE_CONFIG, DefaultLocaleConfig } from './daterangepicker.config';
+import { LOCALE_CONFIG, DefaultLocaleConfig, LocaleConfig } from './daterangepicker.config';
 
 @Injectable()
 export class LocaleService {
-  constructor(@Inject(LOCALE_CONFIG) private _config) {}
+  constructor(@Inject(LOCALE_CONFIG) private _config: LocaleConfig) {}
 
   get config() {
     if (!this._config) {
