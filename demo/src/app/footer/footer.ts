ce60d5f2fb49827fbe6cc64fe66d54940ebfa4a3
Nils Tijtgat <nils.tijtgat@showpad.com>
2020-03-30 19:57:30 +0200

    Make version in footer dynamic

--- demo/src/app/footer/footer.ts
+++ demo/src/app/footer/footer.ts
@@ -1,11 +1,14 @@
 import { Component, NgModule } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.html',
     styleUrls: ['./footer.scss'],
 })
-export class Footer {}
+export class Footer {
+    version = environment.version;
+}
 
 @NgModule({
     exports: [Footer],
--- demo/src/environments/environment.ts
+++ demo/src/environments/environment.ts
@@ -3,6 +3,9 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+import { version } from '../../../package.json';
+
 export const environment = {
+    version,
     production: false,
 };
