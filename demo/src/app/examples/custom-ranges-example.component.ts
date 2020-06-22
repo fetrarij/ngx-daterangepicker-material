61f72031a913f06f4b0c642bc54364de84fc08d7
Akan Yilmaz <captain_akan_91@hotmail.com>
2020-06-22 11:22:48 +0200

    Update custom-ranges-example.component.ts
    This would be the correct Today and Yesterday ranges including the time.

--- demo/src/app/examples/custom-ranges-example.component.ts
+++ demo/src/app/examples/custom-ranges-example.component.ts
@@ -54,8 +54,8 @@ export class CustomRangesExampleComponent {
         { date: moment().add(2, 'days'), text: 'Yeeeees!!!' },
     ];
     ranges = {
-        Today: [moment(), moment()],
-        Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        Today: [moment().startOf('day'), moment().endOf('day')],
+        Yesterday: [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')],
         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
         'This Month': [moment().startOf('month'), moment().endOf('month')],
