2d011797bd55e51af7f5670a30135d3389003e1b
Fetrarijaona R <fetrarij@gmail.com>
2020-07-03 15:39:47 +0300

    chore(theme): fix prebuilt theme #295 #266

--- schematics/ng-add/setup-project.ts
+++ schematics/ng-add/setup-project.ts
@@ -6,9 +6,11 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { chain, Rule, Tree } from '@angular-devkit/schematics';
-import { addModuleImportToRootModule, getProjectFromWorkspace } from '@angular/cdk/schematics';
+import { chain, Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
+import { addModuleImportToRootModule, getProjectFromWorkspace, getProjectStyleFile } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/config';
+import {Schema} from './schema';
+import {addThemeToAppStyles } from './theming/theming';
 
 const moduleName = 'NgxDaterangepickerMd';
 
@@ -18,8 +20,12 @@ const moduleName = 'NgxDaterangepickerMd';
  *  - Adds pre-built themes to styles.ext
  *  - Adds Browser Animation to app.module
  */
-export default function (options: any): Rule {
-    return chain([addDateRangePickerModule(options)]);
+export default function (options: Schema): Rule {
+    return chain([
+        addDateRangePickerModule(options),
+        addThemeToAppStyles(options),
+        addMaterialAppStyles(options)
+    ]);
 }
 
 /**
@@ -35,5 +41,46 @@ function addDateRangePickerModule(options: any) {
         addModuleImportToRootModule(host, moduleName + '.forRoot()', 'ngx-daterangepicker-material', project);
 
         return host;
+    };
+}
+/**
+ * Adds custom Material styles to the project style file. The custom CSS sets up the Roboto font
+ * and reset the default browser body margin.
+ */
+function addMaterialAppStyles(options: Schema) {
+    return (host: Tree, context: SchematicContext) => {
+      const workspace = getWorkspace(host);
+      const project = getProjectFromWorkspace(workspace, options.project);
+      const styleFilePath = getProjectStyleFile(project);
+      const logger = context.logger;
+
+      if (!styleFilePath) {
+        logger.error(`Could not find the default style file for this project.`);
+        logger.info(`Please consider manually setting up the Roboto font in your CSS.`);
+        return;
+      }
+
+      const buffer = host.read(styleFilePath);
+
+      if (!buffer) {
+        logger.error(`Could not read the default style file within the project ` +
+          `(${styleFilePath})`);
+        logger.info(`Please consider manually setting up the Robot font.`);
+        return;
+      }
+
+      const htmlContent = buffer.toString();
+      const insertion = '\n' +
+        `html, body { height: 100%; }\n` +
+        `body { margin: 0; font-family: Roboto, "Helvetica Neue", sans-serif; }\n`;
+
+      if (htmlContent.includes(insertion)) {
+        return;
+      }
+
+      const recorder = host.beginUpdate(styleFilePath);
+
+      recorder.insertLeft(htmlContent.length, insertion);
+      host.commitUpdate(recorder);
     };
 }
--- tools/build-themes.sh
+++ tools/build-themes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEST_PATH=demo/src/assets
+DEST_FINAL_PATH=tools/prebuilt-themes/
 INPUT_PATH=$DEST_PATH/custom-themes/
 
 
@@ -14,6 +15,7 @@ do
   FILENAME=${FILE#$INPUT_PATH}
   BASENAME=${FILENAME%.scss}
   $(npm bin)/node-sass $FILE > $DEST_PATH/$BASENAME.css
+  cp $DEST_PATH/$BASENAME.css $DEST_FINAL_PATH/$BASENAME.css
 done
 
 echo Finished building CSS.
