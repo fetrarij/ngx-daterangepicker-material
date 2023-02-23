e6aa526f3668fd766482e28e2fb70749b20aab5e
RAWAT <suraj.rawat.ext@siemens.com>
2023-02-23 16:28:32 +0530

    added guard against timepickerVariables object values during getDateWithTime function call on click of OKAY button while selecting date range. here is bug link for the same https://github.com/fetrarij/ngx-daterangepicker-material/issues/514

--- src/daterangepicker/daterangepicker.component.ts
+++ src/daterangepicker/daterangepicker.component.ts
@@ -1412,19 +1412,24 @@ export class DaterangepickerComponent implements OnInit, OnChanges {
    * @param side left or right
    */
   private getDateWithTime(date, side: SideEnum): dayjs.Dayjs {
-    let hour = parseInt(String(this.timepickerVariables[side].selectedHour), 10);
-    if (!this.timePicker24Hour) {
-      const ampm = this.timepickerVariables[side].ampmModel;
-      if (ampm === 'PM' && hour < 12) {
-        hour += 12;
+    if(!!this.timepickerVariables[side]) {
+      let hour = parseInt(String(this.timepickerVariables[side].selectedHour), 10);
+      if (!this.timePicker24Hour) {
+        const ampm = this.timepickerVariables[side].ampmModel;
+        if (ampm === 'PM' && hour < 12) {
+          hour += 12;
+        }
+        if (ampm === 'AM' && hour === 12) {
+          hour = 0;
+        }
       }
-      if (ampm === 'AM' && hour === 12) {
-        hour = 0;
-      }
+      const minute = parseInt(String(this.timepickerVariables[side].selectedMinute), 10);
+      const second = this.timePickerSeconds ? parseInt(String(this.timepickerVariables[side].selectedSecond), 10) : 0;
+      return date.clone().hour(hour).minute(minute).second(second);
+  
+    }else{
+      return;
     }
-    const minute = parseInt(String(this.timepickerVariables[side].selectedMinute), 10);
-    const second = this.timePickerSeconds ? parseInt(String(this.timepickerVariables[side].selectedSecond), 10) : 0;
-    return date.clone().hour(hour).minute(minute).second(second);
   }
 
   /**
