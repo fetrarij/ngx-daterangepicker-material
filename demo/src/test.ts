26ff55f3d321dce41a30a067d263c2a7dc3fbae6
Fetra Randria <fetrarij@gmail.com>
2026-08-25 18:41:15 +0300

    fix(test): remove obsolete require.context bootstrap breaking Chrome

--- demo/src/test.ts
+++ demo/src/test.ts
@@ -4,16 +4,6 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
-
-// First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false }
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
-// load our package
-const context2 = require.context('../../src', true, /\.spec\.ts/);
-context2.keys().map(context2);
